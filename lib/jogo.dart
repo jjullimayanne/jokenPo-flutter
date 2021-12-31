954111af1c95044ab5ab22d421fffc7d3e7e3b5d
Julli Mayanne <julli.mayanne@gruposbf.com.br>
2021-12-31 15:31:40 -0300

    gestures

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Jogo extends StatefulWidget {
@@ -10,15 +12,68 @@ class Jogo extends StatefulWidget {
 class _JogoState extends State<Jogo> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       appBar: AppBar(
         title: Text("JokenPo"),
       ),
-      body: Column(
-        children: [
-          
-        ],
-      ) ,
+      body: Container(
+        decoration:
+            BoxDecoration(border: Border.all(width: 3, color: Colors.black)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha do App",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            
+            GestureDetector(
+              onTap: () {
+                print("imagen clilcada");
+              },
+              onDoubleTap: () {
+                print("Dois cliques");
+              },
+              child: Image.asset("images/padrao.png"),
+              onLongPress: () {
+                print("longo clique ");
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(
+                "Escolha uma opção abaixo",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  "images/padrao.png",
+                  height: 100,
+                ),
+                Image.asset(
+                  "images/padrao.png",
+                  height: 100,
+                ),
+                Image.asset(
+                  "images/padrao.png",
+                  height: 100,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
